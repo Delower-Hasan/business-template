70bb75444f13e7fe12e27babd47afc22e6cedf9a
Delower Hasan <dhhasansaha11@gmail.com>
2020-06-29 20:37:02 +0600

    project submitted for the first time

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$('.banner-img').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+ 
+$('.client_img').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:true,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+ 
